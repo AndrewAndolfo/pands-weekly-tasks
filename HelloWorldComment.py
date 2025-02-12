9d9e0a006ce222868d789eb8d2020146104db42c
Win11 <andolfo.andrew@gmail.com>
2025-02-12 04:13:00 +0000

    pands-weekly-tasks

--- HelloWorldComment.py
+++
@@ -1,9 +0,0 @@
-# Hello World
-# This program just print Hello World
-# Andrew Andolfo
-print "Hello World"
-
-# To run this program in Cmder, I have to go to my directory where is this program:
-# cd C:\Principles of Data Analytics\Pands\pands-weekly-tasks
-# Then I type:
-# python HelloWorldComment.py
--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
-print("Hello World")
+# Hello World
+# This program just print Hello World
+# Andrew Andolfo
+print ("Hello World")
+
+# To run this program in Cmder, I have to go to my directory where is this program:
+# cd C:\Principles of Data Analytics\Pands\pands-weekly-tasks
+# Then I type:
+# python HelloWorld.py
