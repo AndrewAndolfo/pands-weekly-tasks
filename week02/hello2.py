6d57452fa5d5e3a417c7d412536330b160602e12
Win11 <andolfo.andrew@gmail.com>
2025-02-13 11:45:16 +0000

    week02

--- week02/hello2.py
+++
@@ -1,7 +0,0 @@
-# hello2
-# This program reads the name imputed and answer "Hello, 'person name'"
-# Andrew Andolfo
-
-name = input('Enter your name:') 
-print (f'Hello {name}.\nNice to meet you.') # It's important to leave a space after 'Hello' to separate 'Hello' and 'name'.
-# If there is no space, the output would be "Helloname".
